4d603b0c38baa5932d80e5a29d187464eab98c53
anhtay <tayphongtran@gmail.com>
2022-03-14 10:24:31 +0700

    init

---
+++ student.js
@@ -0,0 +1,26 @@
+class Student{
+    constructor(id, name, age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    getId(){
+        return this.id;
+    }
+    setId(id){
+        this.id = id
+    }
+    getId(){
+        return this.name;
+    }
+    setId(name){
+        this.name = name;
+    }
+    getId(){
+        return this.id;
+    }
+    setId(age){
+        this.age = age;
+    }
+}
